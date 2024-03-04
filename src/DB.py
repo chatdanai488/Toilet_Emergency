b01776f81b9ddb216981710afaa8c376878d1253
RandomNickUnknown <147534459+RandomNickUnknown@users.noreply.github.com>
2024-03-04 22:19:13 +0800

    DB OK

--- src/DB.py
+++ src/DB.py
@@ -4,31 +4,38 @@ class DB:
     def __init__ (self):
         self.connection = pyodbc.connect(
             'DRIVER={ODBC Driver 17 for SQL Server};'
-            'SERVER=LAPTOP-HDSHPKBA\TOILET_EMERGENCY;'
+            'SERVER=LAPTOP-HDSHPKBA\\TOILET_EMERGENCY;'
             'DATABASE=dbo;'
             'UID=TOILETMASTER;'
             'PWD=POOP123;'
         )
+
     
     def Fetch(self):
         # Create a cursor
-        cursor = self.connection.cursor()
+        with self.connection.cursor() as cursor:
+            # Execute a SQL query
+            cursor.execute("SELECT * FROM dbo.LocTb")
 
-        # Execute a SQL query
-        cursor.execute("SELECT * FROM table_name")
+            # Fetch results
+            rows = cursor.fetchall()
 
-        # Fetch results
-        rows = cursor.fetchall()
+            # Process results
+            for row in rows:
+                print(row)
 
-        # Process results
-        for row in rows:
-            print(row)
-
-        # Close the cursor and connection
-        cursor.close()
-        self.connection.close()
+        
     
+    def Insert(self):
+        
+        with self.connection.cursor() as cursor:
+            cursor.execute("INSERT INTO dbo.LocTb (fDate, fStdId, fLat, fLong, fAlt, fImg, fPlace, fStat) VALUES ('2024-03-04', 'ABC12345', 40.7128, -74.0060, 10.0, 'image.jpg', 'New York', '1');")
+
+            
+
+    
+
 if __name__ == "__main__":
     Database = DB()
     Database.Fetch()
-    
+    Database.Insert()
